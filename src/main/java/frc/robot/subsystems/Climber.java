0d1b89a809d50b6f260ac5450702969fdd6c7a89
northwoodrobotics-user <43829148+northwoodrobotics-user@users.noreply.github.com>
2020-11-19 13:40:12 -0500

    change name from robotlifter to climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * Add your docs here.
  */
-public class RobotLifter extends Subsystem {
+public class Climber extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
